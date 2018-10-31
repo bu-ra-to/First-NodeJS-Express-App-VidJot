58f17245ecfa2709606051cf5ad63aba5afb0f94
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-10-30 21:06:59 -0400

    config ignored

--- config/database.js
+++
@@ -1,6 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: 'mongodb://IvanFirstApp:KraKpRo23@ds143603.mlab.com:43603/ivanzagrebelnyy1' };
-}
-else {
-    module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' };
-}
